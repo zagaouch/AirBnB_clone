5e8c21d659aca2f9de33e89f658f251db931051a
zagaouch <i.zagaouch@gmail.com>
2023-10-15 18:18:54 +0100

    task1

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,33 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-
-import uuid
+"""
+Contains class BaseModel
+"""
 from datetime import datetime
-import models
+import uuid
 
 
 class BaseModel:
-    """Represents the BaseModel of the AirBnB project."""
-
-    def __init__(self, *args, **kwargs):
-        """initialize an instance"""
-        dateformat = '%Y-%m-%dT%H:%M:%S.%f'
+    """
+    BaseModel class
+    """
+    def __init__(self):
+        """
+        Initializes BaseModel class
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.strptime(value, dateformat)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """
-        Return the dictionary of the BaseModel instance.
-        """
-        data = self.__dict__.copy()
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
-        data['__class__'] = self.__class__.__name__
-        return data
-
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clsname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
+        """
+        Returns string representation of BaseModel class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        """
+        update the public instance attribute updated_at
+        """
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
